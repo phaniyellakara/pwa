bc299dfa7f86c9233874236a666793dd0b8a2c1c
pyzen5 <pyzen55@gmail.com>
2020-07-31 23:49:14 +0530

    changes

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,3 @@
-self.addEventListener("install", (event) => {
-  console.log("Service worker installing...");
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (event) => {
-  console.log("Service worker activating...");
-});
-
-// I'm a new service worker
-
-self.addEventListener("fetch", (event) => {
-  console.log("Fetch intercepted for:", event.request.url);
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-      return fetch(event.request);
-    })
-  );
-});
-
 self.addEventListener("notificationclose", (event) => {
   const notification = event.notification;
   const primaryKey = notification.data.primaryKey;
